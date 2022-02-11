bf5282966259957ff83e09a2528832aa5b196dc2
Erick Harano <erick.harano@crmall.com>
2022-02-11 15:53:10 -0300

    adjust class

--- lib/app/shared/presenter/timer/screen_inactive.dart
+++ lib/app/shared/presenter/timer/screen_inactive.dart
@@ -26,3 +26,47 @@ class ScreenInactive {
     _start.cancel();
   }
 }
+
+
+// EXEMPLO DE TELA COM TIMER
+
+// import 'package:flutter/material.dart';
+// import '../../../shared/presenter/timer/screen_inactive.dart';
+
+// class PageExample extends StatefulWidget {
+//   const PageExample({Key? key}) : super(key: key);
+
+//   @override
+//   State<PageExample> createState() => _PageExample();
+// }
+
+// class _PageExample extends State<PageExample> {
+//   late ScreenInactive screenInactive;
+
+//   void _timerCallback() {
+//     screenInactive = ScreenInactive(route: "/two", seconds: 5);
+//     screenInactive.start();
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _timerCallback();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTapDown: (tapDown) {
+//         if (screenInactive != null) {
+//           screenInactive.cancel();
+//         }
+//         _timerCallback();
+//       },
+//       behavior: HitTestBehavior.translucent,
+//       child: const Center(
+//         child: Text("Page Four"),
+//       ),
+//     );
+//   }
+// }
